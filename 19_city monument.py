c8b23e9b4535e9ee76f399a9d94e18899c9c7677
priyankatikle <135009955+priyankatikle@users.noreply.github.com>
2023-05-30 14:26:33 +0530

    Add files via upload

---
+++ 19_city monument.py
@@ -0,0 +1,10 @@
+#enter the city name and check the monument of that city
+city=input("Enter the city for monument:")
+if(city=="Delhi"):
+    print("Monument of",city,"is Redfort")
+elif(city=="Agra"):
+    print("Monument of",city,"is Taj Mahal")
+elif(city=="Jaipur"):
+    print("Monument of",city,"is Jal Mahal")
+else:
+    print("Wrong Input")
