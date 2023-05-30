d60809734660dfd9329a9de4faab18282d870f62
priyankatikle <135009955+priyankatikle@users.noreply.github.com>
2023-05-30 14:22:08 +0530

    Add files via upload

---
+++ 13_triangle.py
@@ -0,0 +1,14 @@
+s1=int(input("Enter the side1"))
+s2=int(input("Enter the side2"))
+s3=int(input("Enter the side3"))
+a1=int(input("Enter the angle1"))
+a2=int(input("Enter the angle2"))
+a3=int(input("Enter the angle3"))
+all_side=180
+if(a==b or a==c or b==c):
+elif(all_side=a1+a2+a3):
+elif:(a==b or a==c or b==c):
+    print("isoscalas triangle")
+else:
+    print("Not isoscalas triangle")
+             
---
+++ 1_palindrome.py
@@ -0,0 +1,13 @@
+#palindrome
+num=int(input("Enter the number:"))
+r=0
+t=num
+while(num!=0):
+    rem=int(num%10)
+    r=(r*10)+rem
+    num=int(num/10)
+print(r)
+if(t==r):
+    print("palindrome")
+else:
+    print("Not palindrome")
